960742ca108e3629692fb6cca8398a2745064f03
alsino <iamahorse@gmail.com>
2019-07-09 16:20:20 +0200

    Mobile fix

---
+++ basic_Javascript/script.js
@@ -0,0 +1,15 @@
+let myName = "Alsino";
+let myAge = 35;
+let isMarried = false;
+let myFavoriteColor = "#fff";
+let myFavoriteAnimals = ["Deer", "Panda", "Giraffe", "Cat"];
+
+// console.log(myFavoriteAnimals);
+// console.log(myFavoriteAnimals.length);
+// console.log(myFavoriteAnimals[myFavoriteAnimals.length - 1]);
+
+let sentence = `Hello, my name is ${myName}. And my age is ${myAge}. My favorite animal is ${myFavoriteAnimals[0]}.`
+
+console.log(sentence);
+
+

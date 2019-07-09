5611bf29c5ff5497dc697d44618c1051a5704fdb
alsino <iamahorse@gmail.com>
2019-07-09 16:24:52 +0200

    Mobile fix

--- myMobileNet_upload/sketch.js
+++ myMobileNet_upload/sketch.js
@@ -98,7 +98,7 @@ function setup() {
   bullshitBtn = createButton("Generate AI art critique");
   bullshitBtn.parent('wrapper');
   bullshitBtn.class('aiText');
-  bullshitBtn.style('display', 'none');
+  bullshitBtn.style('display', 'block');
   
   canvas = createCanvas(windowWidth,600);
   // canvas.parent('wrapper');
